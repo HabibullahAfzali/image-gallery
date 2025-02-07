353b7f62a0aaf8d0e0d1ae2a790670f56dfd9d41
HabibAfzali <82744410+HabibullahAfzali@users.noreply.github.com>
2025-02-07 22:06:25 +0100

    ImageController:getAllImages,addImage,deleteImage and updateImage method are created and crud operation are done!

--- backend/src/main/java/com/imagegallery/image_gallery/controller/ImageController.java
+++ backend/src/main/java/com/imagegallery/image_gallery/controller/ImageController.java
@@ -15,5 +15,39 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 public class ImageController {
 
+	@Autowired
+	private ImageRepository imageRepository;
 
+	@GetMapping
+	public List<Image> getAllImages(){
+
+		return  imageRepository.findAll();
+	}
+
+	@PostMapping
+	public Image addImage(@RequestBody Image image){
+
+		return imageRepository .save(image);
+
+	}
+	@DeleteMapping("/{id}")
+
+	public void deleteImage(@PathVariable Long id){
+		imageRepository.deleteById(id);
+	}
+
+	@PutMapping("/{id}")
+
+	public  Image updateImage(@PathVariable Long id, @RequestBody Image newImage){
+
+
+		return imageRepository.findById(id).map(image -> {
+			image.setTitle(newImage.getTitle());
+			image.setUrl(newImage.getUrl());
+			return imageRepository.save(image);
+		}).orElseGet(() ->{
+			newImage.setId(id);
+			return imageRepository.save(newImage);
+		});
+	}
 }
